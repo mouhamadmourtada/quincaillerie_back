744a3185709793385cb94cfbaf5421a5445dccfc
=Mouhamad Mourtada DIOP <=>
2024-12-20 18:35:09 +0000

    k

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ sequelize.authenticate()
         return syncDatabase();
     })
     .then(() => {
-        const PORT = process.env.PORT || 3005;
+        const PORT = process.env.PORT || 3000;
         app.listen(PORT, () => {
             console.log(`Server is running on port ${PORT}`);
         });
